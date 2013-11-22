4bcbd6f0106b60a6201d8aafd429eee409252300
Jørgen <jorgen.braseth@gmail.com>
2013-11-23 00:12:03 +0100

    Pulled out format to a separate file. Made loading for format-file work. Page styling updated. Added display of filenames for loaded files.

--- src/main/webapp/js/flatfiledoc.js
+++ src/main/webapp/js/flatfiledoc.js
@@ -1,74 +1,6 @@
-var lineDefs = [
-//    {
-//        pattern: /ENH .*/i,
-//        lines: [
-//            { start: 0, end: 3, description: "Identifiserer start enhet  record" },
-//            { start: 4, end: 12, description: "Organisasjonsnummer" },
-//            { start: 13, end: 16, description: "Enhetstype" },
-//            { start: 17, end: 17, description: "Hovedsakstype" },
-//            { start: 18, end: 21, description: "Undersakstype" },
-//            { start: 22, end: 29, description: "Dato hvor enheten ble innført som ny i ER" },
-//            { start: 30, end: 37, description: "Dato for siste endring på enheten" },
-//            { start: 38, end: 38, description: "Første overføring" },
-//            { start: 39, end: 47, description: "Korrekt orgnr" },
-//            { start: 48, end: 48, description: "Type overføring" }
-//        ]
-//    },
-//    {
-//        pattern: /TRAI.*/i,
-//        lines: [
-//            { start: 0, end: 3, description: "Felttype: Trailer record" },
-//            { start: 4, end: 6, description: "Avsender: ER er avsender" },
-//            { start: 7, end: 13, description: "Antall enheter overført" },
-//            { start: 14, end: 22, description: "Antall records overført" }
-//        ]
-//    },
-    {
-        pattern: /HEAD.*/i,
-        lines: [
-//            { start: 0, end: 3, description: "Felttype: Header record" },
-            { start: 4, end: 6, description: "Avsender: ER er avsender" },
-            { start: 7, end: 14, description: "ÅÅÅÅMMDD		Dato for generering av filen." },
-//            { start: 15, end: 19, description: "Kjørenr.\nLedende 0. Hver overføring nummereres fortløpende.\nDet skal ikke være hull i nummerserien." },
-            { start: 20, end: 22, description: "Tilsvarende avsender i batch in." },
-            { start: 23, end: 23, description: "A = Ordinær ajourholdsdata utveksling.\nS =  Data bestilt via \"SKD knappen\"\nK = Knytningsfil med begrensede enhetsdata" }
-        ]
-    }
-//    {
-//        pattern: /USYS.*/i,
-//        lines: [
-//            { start: 0, end: 3, description: "Status: Ubemannet virksomhet" },
-//            { start: 4, end: 4, description: "Opplysningen er: Ny eller endret / Utgått / Kopi av tidligere sendt oppl." },
-//            { start: 5, end: 7, description: "Kode for avsender som sist endret oppl." }
-//        ]
-//    },
-//    {
-//        pattern: /LEDE.*/i,
-//        lines: [
-//            { start: 0, end: 3, description: "Felttype" },
-//            { start: 4, end: 4, description: "N/U/K	Opplysningen er: Ny eller endret / Utgått / Kopi av tidligere sendt oppl." },
-//            { start: 5, end: 7, description: "Endret av" },
-//            { start: 8, end: 8, description: "Indikerer rolle" },
-//            { start: 9, end: 9, description: "Indikerer at dette er data for rollen (I motsetning til fritekst)" },
-//            { start: 10, end: 39, description: "Ansvarsandel" },
-//            { start: 40, end: 40, description: "F/N/K/R \nF = Egenfratreden\nN = Ikke fratrådt\nK = Fratrådt pga konkurskarantene\nR = Fratrådt pga rettighetstap" },
-//            { start: 41, end: 44, description: "Valgtav" },
-//            { start: 45, end: 47, description: "Presentasjonsrekkefølge av styremedlemmer osv." },
-//            { start: 48, end: 58, description: "Fødselsnr eller Dnr." },
-//            { start: 59, end: 108, description: "Fornavn" },
-//            { start: 109, end: 158, description: "mellomnavn" },
-//            { start: 159, end: 208, description: "slektsnavn" },
-//            { start: 209, end: 217, description: "Postnr" },
-//            { start: 218, end: 252, description: "Adresse1" },
-//            { start: 253, end: 287, description: "Adresse2" },
-//            { start: 288, end: 322, description: "Adresse3" },
-//            { start: 323, end: 325, description: "Adresselandkode" },
-//            { start: 326, end: 326, description: "Personstatus" }
-//        ]
-//    }
-];
-
-
+var definitionLoaded = false;
+var dataLoaded = false;
+var lineDefs;
 
 function parseDocument(raw) {
     var lines = raw.split("\n");
@@ -82,11 +14,10 @@ function parseDocument(raw) {
 
 function renderLine(raw, lineData) {
     var htmlLine = $("<span class='line'></span>");
-
     if(!$.isEmptyObject(lineData)){
-        $.each(lineData, function(description, value){
+        $.each(lineData, function(idx, value){
             var token = $("<span class='token'>" + value.text + "</span>");
-            if(value.description == undefined) {
+            if(value.unknown) {
                 token.addClass("unknownFormat");
             }
             htmlLine.append(token);
@@ -111,7 +42,7 @@ function addUndefinedRanges(lineDefinition,rawLine) {
 
         while(curIdx < lastIndex) {
             if(startIndexes[intervalIdx] > curIdx) {
-                allRanges = allRanges.concat({start: curIdx, end: startIndexes[intervalIdx]-1})
+                allRanges = allRanges.concat({start: curIdx, end: startIndexes[intervalIdx]-1, unknown:true})
             }
             allRanges = allRanges.concat(lineDefinition[intervalIdx]);
             curIdx = endIndexes[intervalIdx] + 1;
@@ -121,38 +52,26 @@ function addUndefinedRanges(lineDefinition,rawLine) {
     return allRanges;
 }
 function linedata(rawLine) {
-    var lineDefinition = getLineDefinitionFor(rawLine, lineDefs);
-    lineDefinition = addUndefinedRanges(lineDefinition, rawLine);
-    if(lineDefinition){
+    var fieldDescriptions = getLineDefinitionFor(rawLine, lineDefs);
+    fieldDescriptions = addUndefinedRanges(fieldDescriptions, rawLine);
+    if(fieldDescriptions){
         var data = [];
-        $(lineDefinition).each(function(lineNo, lineDef){
-            var tokenText = rawLine.substr(lineDef.start, lineDef.end - lineDef.start + 1);
-            if(tokenText != undefined) {
-                data = data.concat({description: lineDef.description, text: tokenText.trim()});
-            }else{
-                data = data.concat(undefined);
+        $(fieldDescriptions).each(function(fieldNo, fieldDescription){
+            if(fieldDescription){
+                var fieldValue = rawLine.substr(fieldDescription.start, fieldDescription.end - fieldDescription.start + 1);
+                if(fieldDescription.unknown) {
+                    data = data.concat({text: fieldValue, unknown:true});
+                }else{
+                    data = data.concat({description: fieldDescription.description, text: fieldValue, name:fieldDescription.name});
+                }
             }
         });
         return data;
+
     }
 
     return {};
 }
-//
-//function parseLine(rawLine) {
-//    var parsedLine = $("<span class='line'></span>");
-//    var lineDefinition = getLineDefinitionFor(rawLine, lineDefs);
-//    if(lineDefinition){
-//        var tokenized = tokenize(rawLine, lineDefinition);
-//        $(tokenized).each(function(idx, token){
-//            parsedLine.append(token);
-//        });
-//    }else{
-//        parsedLine.html(rawLine);
-//        parsedLine.addClass("unparsed");
-//    }
-//    return  parsedLine
-//}
 
 function hideLineInfo() {
     $("#lineinfo").hide();
@@ -160,17 +79,8 @@ function hideLineInfo() {
 
 function displayLineInfo(data) {
     var infoElement = $("#lineinfo");
-    infoElement.html("");
-    var valuelist = $("<dl></dl>");
-    $.each(data,function(key,value){
-        var valuePair = $("<div class='valuePair'></div>");
-        if(value) {
-            valuePair.append($("<dt>"+key+"</dt>"));
-            valuePair.append($("<dd>"+value+"</dd>"));
-            infoElement.append(valuePair);
-        }
-    });
-    infoElement.append(valuelist);
+    console.log(data);
+    infoElement.html(tooltipTemplate({elements: data}));
     infoElement.show();
 }
 
@@ -196,20 +106,43 @@ function tokenize(line, lineDefs) {
 }
 
 function loadData(file) {
-    var dataHolder = $("#data");
-    dataHolder.addClass("spinner");
+    if(definitionLoaded) {
+        var dataHolder = $("#data");
+        dataHolder.addClass("spinner");
 
-    var file = file[0], reader = new FileReader();
-    reader.onload = function(event) {
-        var parsedDocument = parseDocument(event.target.result);
-        dataHolder.removeClass("spinner");
-        dataHolder.html(parsedDocument);
-        dataHolder.removeClass("unloaded");
-    };
-    reader.readAsText(file);
+        var file = file[0], reader = new FileReader();
+        reader.onload = function(event) {
 
+            var parsedDocument = parseDocument(event.target.result);
+            $("#dataSource").text(file.name);
+            dataHolder.removeClass("spinner");
+            dataHolder.html(parsedDocument);
+            dataHolder.removeClass("unloaded");
+
+        };
+        reader.readAsText(file);
+
+        dataLoaded = true;
+    }
     return false;
 }
+function loadDefinition(file) {
+    var definitionHolder = $("#definition");
+    var file = file[0], reader = new FileReader();
+    reader.onload = function(event) {
+        var loadedDefs = eval(event.target.result);
+        $("#definitionSource").text(file.name);
+        var definition = $("#definition");
+        definition.text(event.target.result);
+        definition.removeClass("unloaded");
+        definition.animate({"height":200},1000);
+        lineDefs = loadedDefs;
+        definitionLoaded=true;
+    };
+    reader.readAsText(file);
+    return false;
+}
+
 function makeFileDropTarget(elm, onDropFunction) {
     elm.on('dragover', function(e) {
         elm.addClass("dragAllowed");
@@ -224,22 +157,28 @@ function makeFileDropTarget(elm, onDropFunction) {
 
     elm.on('dragleave', function(e) {
         elm.removeClass("dragAllowed");
-        console.log("aaahw left!");
         e.preventDefault();
         e.stopPropagation();
     });
 
     elm.on('drop', function(e){
         elm.removeClass("dragAllowed");
-        onDropFunction(e);
+        if(e.originalEvent.dataTransfer){
+                    if(e.originalEvent.dataTransfer.files.length) {
+                        e.preventDefault();
+                        e.stopPropagation();
+                        onDropFunction(e.originalEvent.dataTransfer.files);
+                    }
+                }
     });
 }
-
 
 function makeInfoTooltip(){
     var tooltip = $("<div id='lineinfo'></div>");
     var body = $("html");
     body.append(tooltip);
+
+    tooltipTemplate = Handlebars.compile($("#tooltip-template").html());
 
     body.on("mousemove",function(e){
         var x = e.pageX;
@@ -269,15 +208,6 @@ function preventFileDropOutsideTargets() {
 $(function(){
     preventFileDropOutsideTargets();
     makeInfoTooltip();
-
-    var dataReceiver = $("#data");
-    makeFileDropTarget(dataReceiver,function(e){
-        if(e.originalEvent.dataTransfer){
-            if(e.originalEvent.dataTransfer.files.length) {
-                e.preventDefault();
-                e.stopPropagation();
-                loadData(e.originalEvent.dataTransfer.files);
-            }
-        }
-    });
+    makeFileDropTarget($("#data"),loadData);
+    makeFileDropTarget($("#definition"),loadDefinition);
 });
